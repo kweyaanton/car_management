3339cc28baa920e069760d408e36f083b4fcc014
kweyaanton <kweyaanton@gmail.com>
2023-05-17 14:16:16 +0300

    google cloud

---
+++ my_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\items\projects\vehicle_service_management_django\my_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
